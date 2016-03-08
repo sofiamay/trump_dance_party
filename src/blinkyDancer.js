d5f89650c54decdf93d5f913de6b6bf1bc64ce9b
Hack Reactor Students <accounts@hackreactor.com>
2016-03-07 19:45:46 -0800

    initialize repo

---
+++ src/blinkyDancer.js
@@ -0,0 +1,49 @@
+var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+ 
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+makeBlinkyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  makeDancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  
+  this.$node.toggle();
+};
+
+
+// Walldancer subclass
+
+var Walldancer = function(top, left, timeBetweenSteps) {
+
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  console.log(top);
+  this.$node = $('<span class="trump_wall"></span>');
+};
+
+Walldancer.prototype = Object.create(makeDancer.prototype);
+Walldancer.prototype.constructor = Walldancer;
+Walldancer.prototype.step = function() {
+  makeDancer.prototype.step.call(this);
+};
+
+// Floordancer subclass
+var Floordancer = function(top, left, timeBetweenSteps) {
+
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="trump_floor"></span>');
+  
+};
+
+Floordancer.prototype = Object.create(makeDancer.prototype);
+Floordancer.prototype.constructor = Floordancer;
+Floordancer.prototype.step = function() {
+  makeDancer.prototype.step.call(this);
+};
+
---
+++ src/init.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  window.dancers = [];
+
+
+  // create two separe on click function that represent two different buttons
+    // each button will create a new dancer on either the wall or floor
+
+  $('.addWalldancer').on('click', function(event) {
+     
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+
+    // get the maker function for the kind of dancer we're supposed to make
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    // make a dancer with a random position
+    var dancer = new dancerMakerFunction(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
+      Math.random() * 1000
+    );
+    $('body').append(dancer.$node);
+  });
+
+  $('.addFloordancer').on('click', function(event) {
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+
+    // get the maker function for the kind of dancer we're supposed to make
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    // make a dancer with a random position
+    var dancer = new dancerMakerFunction(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
+      Math.random() * 1000
+    );
+    $('body').append(dancer.$node);
+  });
+});
